bddf96e45304c97a26b1d5708f2b9583ad061bfa
vuongnghia1706 <85352981+vuongnghia1706@users.noreply.github.com>
2025-09-18 17:43:48 +0700

    config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Con lợn à!',
+    introTitle: 'Nhóc tỳ kiaaaa!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao nhóc cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Chết rồi! Đừng bấm vào nhá',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích e mất rồi <3',
+    btnYes: 'Thích a mất rồi <3',
     btnNo: 'Anh em thôi !',
-    question:'Haizz, đã lỡ vậy rồi nhưng người em thích không phải là tui ',
-    btnReply: 'Gửi cho anh trai :(',
-    reply: 'Xin lỗi, anh chỉ là người đến sau :((',
+    question:'Anh cũng biết là em rất iu anh mafaaaa <3 ',
+    btnReply: 'Gửi cho anh iu :(',
+    reply: 'Vợ nhớ mình nhiều lắmmmmm',
     mess: 'Anh biết là vậy mà !',
     messDesc: 'lúc nào rảnh call anh nhé!.',
     btnAccept: 'Anh chờ',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/vuongnghia1706' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
